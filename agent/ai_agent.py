353a9ca4a6abe9e6f82b8fd78812cc38cffdde9f
sosumit001 <contact@sumitso.in>
2025-02-20 11:02:10 +0530

    start: receive-client-move

--- agent/ai_agent.py
+++ agent/ai_agent.py
@@ -1,4 +1,4 @@
-from videosdk import MeetingConfig, VideoSDK
+from videosdk import MeetingConfig, VideoSDK, MeetingEventHandler
 class AIAgent:
     def __init__(self, meeting_id: str, authToken: str, name: str):
         self.meeting_config = MeetingConfig(
@@ -8,11 +8,17 @@ class AIAgent:
             mic_enabled=False,
             webcam_enabled=False
         )
+        # Listen to Meeting Events
+        # subscribe to pubsub topic - GAME_MOVES
+        # receive cliet's response
+        self.ai_agent.add_event_listener()
         self.ai_agent = VideoSDK.init_meeting(**self.meeting_config)
+        
         
     async def join(self):
         await self.ai_agent.async_join()
     
     async def leave(self):
         self.ai_agent.leave()
+    
     
